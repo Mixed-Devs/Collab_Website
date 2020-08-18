da8c98166f15f946e1b9a75709d30ef5af2788e8
Daniil Lebedev <daniilwork247@gmail.com>
2020-08-18 11:45:38 +0000

    Minor cosmetic changes: 1. Added a navbar for the landing age 2. Added a test html file to see how to reference two of them

--- website/website_app/urls.py
+++ website/website_app/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.HomePageView.as_view(), name='home'),
+    path('home/',views.HomePageView.as_view(), name='home'),
+    path('test/',views.TestPageView.as_view(), name='test'),
 
 ]
--- website/website_app/views.py
+++ website/website_app/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class TestPageView(TemplateView):
+    template_name = 'test.html'
